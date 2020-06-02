c515b3567398239e4369a3c8b9458ac6da5e1f1b
bathirenathan <bathirenathan@nmsworks.co.in>
2020-06-02 12:17:16 +0530

    first draft

---
+++ app.js
@@ -0,0 +1,15 @@
+const data = {
+    'Today' : 20,
+    'Last 7 Days' : 190
+}
+
+
+
+class Tiles {
+    constructor(data, element){
+        this.data = data;
+        this.element = element;
+    }
+
+    
+}
