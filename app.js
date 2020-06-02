801248de6771886ac23a3ae4c761894b4fd55595
bathirenathan <bathirenathan@nmsworks.co.in>
2020-06-02 12:28:06 +0530

    added jqyery

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const data = {
-    'Today' : 20,
-    'Last 7 Days' : 190
-}
-
-
+  Today: 20,
+  'Last 7 Days': 190,
+};
 
 class Tiles {
-    constructor(data, element){
-        this.data = data;
-        this.element = element;
-    }
+  constructor (data, element) {
+    this.data = data;
+    this.element = element;
+    console.log (this.data);
 
-    
+    this.buildTiles ();
+  }
+
+  buildTiles () {
+    try {
+      Object.keys (this.data).forEach (key => {
+        let tile = this.getTile (key, this.data[key]);
+        $(this.element).append(tile);
+      });
+    } catch (err) {
+      console.log (err);
+    }
+  }
+
+  getTile(key, value){
+      return $('<div/>', {class : 'tiles-type1'});
+  }
 }
+
+new Tiles (data, '.summary');
